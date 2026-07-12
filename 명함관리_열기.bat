dd1e782b3c1b1ade46cff54a1ee49f7afbd226d9
brianhan0520-dev <brianhan0520@gmail.com>
2026-07-13 08:50:07 +0900

    Delete 명함관리_열기.bat

--- 명함관리_열기.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start /b powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0server.ps1"
-timeout /t 2 /nobreak > nul
-start "" "http://localhost:8760/contacts-viewer.html"
-echo Server is running. Close this window to stop.
-pause > nul
-taskkill /f /im powershell.exe /fi "WINDOWTITLE eq server.ps1" > nul 2>&1
